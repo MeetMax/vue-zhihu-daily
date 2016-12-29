54b5edda0a7fe1d884baaf6adbca01284b5f49fa
Rick Qin <Rick Qin>
2016-12-29 18:59:06 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import App from './App';
+import VueRouter from 'vue-router';
+import vuex from 'vuex';
+import axios from 'axios';
+import routes from './router';
+import store from './store/store';
 
+Vue.use(VueRouter);
+Vue.use(axios);
+
+const router = new VueRouter({
+	mode: 'history',
+  	routes
+});
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+  router,
+  store,
+  axios,
+  render: h => h(App)
+}).$mount('#app');
---
+++ src/router.js
@@ -0,0 +1,14 @@
+import App from './App'
+
+export default [
+	{
+		path:'/header',
+		component:App.components.vHeader,
+		name:'header'
+	},
+	{
+		path:'/hello',
+		component:App.components.Hello,
+		name:'hello'
+	}
+]
